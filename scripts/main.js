a9848ae24121ab4f3add96b5335b7ae04be5f845
unknown <andrasimion99@yahoo.com>
2020-03-12 18:35:13 +0200

    footer si menu

---
+++ scripts/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "20%";
+    document.getElementById("main").style.marginLeft = "20%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
