ebe4e3d9fb1db45b7bd76bf96d4839d76a365921
unknown <andrasimion99@yahoo.com>
2020-03-15 18:46:23 +0200

    fara buton search si meniu responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,11 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "20%";
-    document.getElementById("main").style.marginLeft = "20%";
+function openNav(x) {
+    if (x.matches) {
+        document.getElementById("mySidenav").style.width = "45%";
+        document.getElementById("main").style.marginLeft = "45%";
+    } else {
+        document.getElementById("mySidenav").style.width = "20%";
+        document.getElementById("main").style.marginLeft = "20%";
+    }
 }
 
 function closeNav() {
