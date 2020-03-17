ba5afebf457fab9c8a5aef5096b09f248f469f3f
unknown <andrasimion99@yahoo.com>
2020-03-17 12:08:56 +0200

    load more cu js pur

--- scripts/articles.js
+++ scripts/articles.js
@@ -1,10 +1,24 @@
-$(document).ready(function () {
-    $(".article").slice(0, 8).show();
-    $("#loadMore").on("click", function (e) {
-        e.preventDefault();
-        $(".article:hidden").slice(0, 8).slideDown();
-        if ($(".article:hidden").length == 0) {
-            $("#loadMore").text("No more content").addClass("noContent");
+function loadMore() {
+    var articlesArr = document.getElementsByClassName('article');
+    var slicedArr = [];
+    var i = 0;
+    for (i = 0; i < 8; i++) {
+        slicedArr.push(articlesArr[i]);
+        slicedArr[i].style.display = "block"
+    }
+    document.getElementById("loadMore").addEventListener('click', function (event) {
+        event.preventDefault();
+        var nextElements = [];
+        for (var j = i; j < i + 8; j++) {
+            if(j<articlesArr.length && window.getComputedStyle(articlesArr[j]).display === "none"){
+                nextElements.push(articlesArr[j]);
+                articlesArr[j].style.display = "block";
+            }
         }
-    });
-})
+        i = j;
+        if(nextElements.length < 8 || articlesArr.length == j){
+            document.getElementById('loadMore').innerText = "No more content";
+            document.getElementById('loadMore').classList.add("noContent");
+        }
+    })
+}
