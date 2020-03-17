1042a4beacde08ce101523c368c7853f75095334
unknown <andrasimion99@yahoo.com>
2020-03-17 15:49:07 +0200

    buton meniu user-friendly

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,20 @@
 function openNav(x) {
-    if (x.matches) {
-        document.getElementById("mySidenav").style.width = "45%";
-        document.getElementById("main").style.marginLeft = "45%";
-    } else {
-        document.getElementById("mySidenav").style.width = "20%";
-        document.getElementById("main").style.marginLeft = "20%";
-    }
-}
+    var sideNav = document.getElementById("mySidenav");
+    var main = document.getElementById("main");
+    if ((sideNav.style.width == 0 && main.style.marginLeft == 0) || (sideNav.style.width == "0px" && main.style.marginLeft == "0px")) {
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+        if (x.matches) {
+            document.getElementById("mySidenav").style.width = "45%";
+            document.getElementById("main").style.marginLeft = "45%";
+            document.getElementsByClassName("main-nav")[0].style.zIndex = "1"
+        } else {
+            document.getElementById("mySidenav").style.width = "20%";
+            document.getElementById("main").style.marginLeft = "20%";
+            document.getElementsByClassName("main-nav")[0].style.zIndex = "1"
+        }
+    } else {
+        document.getElementById("mySidenav").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    }
+
 }
