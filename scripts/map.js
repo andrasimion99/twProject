4649c5e17d1ee862911b56e0c1b1f828e9f38901
unknown <andrasimion99@yahoo.com>
2020-04-07 16:46:19 +0300

    fisiere csv

--- scripts/map.js
+++ scripts/map.js
@@ -6,8 +6,9 @@ document.addEventListener("DOMContentLoaded", function (e) {
       document.getElementById("box").style.display = "block";
       var data = "<div>" + this.getAttribute("data-name") + "</div>";
       data += "<div>" + this.getAttribute("data-capital") + "</div>";
+
       document.getElementById("box").innerHTML = data;
-      document.getElementById("box").style.top = event.offsetY - 30 + "px";
+      document.getElementById("box").style.top = event.offsetY - 50 + "px";
       document.getElementById("box").style.left = event.offsetX + "px";
     });
     countries[i].addEventListener("mouseleave", function (event) {
