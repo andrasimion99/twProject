2e4e914e88d3765f18ab9abf6553ae61222bedd0
unknown <andrasimion99@yahoo.com>
2020-04-14 12:45:45 +0300

    mapa + api

--- api/common/helpers.js
+++ api/common/helpers.js
@@ -1,36 +1,63 @@
-module.exports.validationError = (res, error = 'Data provided is not valid') => {
-    addHeaders(res);
+module.exports.validationError = (
+  res,
+  error = "Data provided is not valid"
+) => {
+  addHeaders(res);
 
-    res.statusCode = 422;
+  res.statusCode = 422;
 
-    res.end(JSON.stringify({
-        status: 'fail',
-            error
-    }, null, 3));
+  res.end(
+    JSON.stringify(
+      {
+        status: "fail",
+        error,
+      },
+      null,
+      3
+    )
+  );
 };
 
-module.exports.error = (res, error = 'An unknown error occurred', statusCode = 500) => {
-    addHeaders(res);
+module.exports.error = (
+  res,
+  error = "An unknown error occurred",
+  statusCode = 500
+) => {
+  addHeaders(res);
 
-    res.statusCode = statusCode;
+  res.statusCode = statusCode;
 
-    res.end(JSON.stringify({
-        status: 'fail',
-        error
-    }, null, 3));
+  res.end(
+    JSON.stringify(
+      {
+        status: "fail",
+        error,
+      },
+      null,
+      3
+    )
+  );
 };
 
 module.exports.success = (res, data = null) => {
-    addHeaders(res);
+  addHeaders(res);
 
-    res.statusCode = 200;
+  res.statusCode = 200;
 
-    res.end(JSON.stringify({
-        status: 'success',
-        data
-    }, null, 3));
+  res.end(
+    JSON.stringify(
+      {
+        status: "success",
+        data,
+      },
+      null,
+      3
+    )
+  );
 };
 
 const addHeaders = (res) => {
-    return res.setHeader('Content-Type', 'application/json');
-}
+  res.setHeader("Content-Type", "application/json");
+  res.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:8125");
+  return res;
+};
