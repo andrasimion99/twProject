b2c39ca0c3d082f5359d24ad138db7a3a17b192d
Raluca Filimon <filimon.raluca@gmail.com>
2020-04-29 13:55:52 +0300

    display chart on front

--- server/view/scripts/chart.js
+++ server/view/scripts/chart.js
@@ -1,192 +1,265 @@
-fetch("http://localhost:3001/api/ethnicity?country=Alaska")
-  .then((data) => {
-    return data.json();
-  })
-  .then((res) => {
-    data = res.data;
-    var minYear = d3.min(data, function (d) {
-      return parseInt(d.Description);
-    });
-    var maxYear = d3.max(data, function (d) {
-      return parseInt(d.Description);
-    });
-    var maxPercent = d3.max(data, function (d) {
-      return parseFloat(d.Data_Value);
-    });
-    var numBars = d3.set(data, function (d) {
-      return d.Stratification1;
-    }).size();
-    console.log(numBars);
+var countries = document.getElementsByClassName("Country");
+var country = "";
 
-    var margin = { left: 100, right: 10, top: 10, bottom: 180 };
-    var yearsArr = [];
-    var width = 600 - margin.left - margin.right;
-    var height = 600 - margin.top - margin.bottom;
-    var barPadding = 20;
-    if(numBars <= 4){
-      barPadding = 40;
+var charts = document.getElementsByClassName("Chart");
+var chart = "";
+
+var series = document.getElementsByClassName("Series");
+var seriesValue = "";
+var seriesName = "";
+
+var api = "";
+
+/*-----Event listener for countries check box------*/
+for (var i = 0; i < countries.length; i++) {
+  countries[i].addEventListener("change", function () {
+    if (this.checked) {
+      country = this.value;
+      disableAll(this, "Country");
+      if (chart == "column-chart" && seriesValue == "All") {
+        api = "http://localhost:3001/api/" + seriesName + "?country=" + country;
+        createChart(api);
+      }
+    } else {
+      country = "";
+      removeDisable(this, "Country");
     }
-    var barWidth = width / numBars - barPadding;
+  });
+}
 
-    var xscale = d3
-      .scaleBand()
-      .domain(
-        data.map(function (d) {
+/*-----Event listener for chart tipe------*/
+
+for (var i = 0; i < charts.length; i++) {
+  charts[i].addEventListener("click", function () {
+    chart = this.value;
+    console.log(chart);
+    this.style.border = "5px solid #2980b9";
+    unclickAll(this);
+    if (chart == "column-chart" && country != "" && seriesValue == "All") {
+      api = "http://localhost:3001/api/" + seriesName + "?country=" + country;
+      createChart(api);
+    } else {
+      d3.select("svg").remove();
+      d3.select("table").remove();
+    }
+  });
+}
+
+function unclickAll(currentChart) {
+  for (var i = 0; i < charts.length; i++) {
+    if (charts[i] != currentChart) {
+      charts[i].style.border = "none";
+    }
+  }
+}
+
+/*-----Event listener for series check box------*/
+for (var i = 0; i < series.length; i++) {
+  series[i].addEventListener("change", function () {
+    if (this.checked) {
+      seriesValue = this.value;
+      seriesName = this.name;
+      disableAll(this, "Series");
+      if (chart == "column-chart" && country != "" && seriesValue == "All") {
+        api = "http://localhost:3001/api/" + seriesName + "?country=" + country;
+        createChart(api);
+      }
+    } else {
+      seriesValue = "";
+      seriesName = "";
+      removeDisable(this, "Series");
+      d3.select("svg").remove();
+      d3.select("table").remove();
+    }
+  });
+}
+
+function disableAll(currentChecked, id) {
+  var elements = document.getElementsByClassName(id);
+  for (var i = 0; i < elements.length; i++) {
+    {
+      if (elements[i] != currentChecked)
+        elements[i].setAttribute("disabled", "");
+    }
+  }
+}
+
+function removeDisable(currentChecked, id) {
+  var elements = document.getElementsByClassName(id);
+  for (var i = 0; i < elements.length; i++) {
+    {
+      if (elements[i] != currentChecked)
+        elements[i].removeAttribute("disabled");
+    }
+  }
+}
+
+function createChart(api) {
+  d3.select("svg").remove();
+  d3.select("table").remove();
+  fetch(api)
+    .then((data) => {
+      return data.json();
+    })
+    .then((res) => {
+      data = res.data;
+      var minYear = d3.min(data, function (d) {
+        return parseInt(d.Description);
+      });
+      var maxYear = d3.max(data, function (d) {
+        return parseInt(d.Description);
+      });
+      var maxPercent = d3.max(data, function (d) {
+        return parseFloat(d.Data_Value);
+      });
+      var numBars = d3
+        .set(data, function (d) {
           return d.Stratification1;
         })
-      )
-      .range([0, width])
-      .paddingInner(0.3)
-      .paddingOuter(0.3);
-    var yscale = d3.scaleLinear().domain([0, maxPercent]).range([height, 0]);
+        .size();
+      console.log(numBars);
 
-    var svg = d3
-      .select("#chart-area")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom);
+      var margin = { left: 100, right: 10, top: 10, bottom: 180 };
+      var yearsArr = [];
+      var width = 500 - margin.left - margin.right;
+      var height = 450 - margin.top - margin.bottom;
+      var barPadding = 20;
+      if (numBars <= 4) {
+        barPadding = 40;
+      }
+      var barWidth = width / numBars - barPadding;
 
-    var g = svg
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    g.append("text")
-      .attr("class", "x-axis-label")
-      .attr("x", width / 2)
-      .attr("y", height + margin.bottom - 20)
-      .attr("font-size", "20px")
-      .attr("text-anchor", "middle")
-      .text("Age Category(years)");
-
-    g.append("text")
-      .attr("class", "y-axis-label")
-      .attr("x", -(height / 2))
-      .attr("y", -60)
-      .attr("font-size", "20px")
-      .attr("text-anchor", "middle")
-      .attr("transform", "rotate(-90)")
-      .text("Obesity Percentage (%)");
-
-    var xAxis = d3.axisBottom(xscale);
-    g.append("g")
-      .attr("class", "x-axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis)
-      .selectAll("text")
-      .attr("y", "10")
-      .attr("x", "-5")
-      .attr("text-anchor", "end")
-      .attr("transform", "rotate(-40)");
-
-    var yAxis = d3.axisLeft(yscale).tickFormat(function (d) {
-      return d + "%";
-    });
-    g.append("g").attr("class", "y-axis").call(yAxis);
-
-    var rects = g
-      .selectAll("rect")
-      .data(
-        data.filter(function (d) {
-          return parseInt(d.Description) === minYear;
-        })
-      )
-      .enter()
-      .append("rect")
-      .attr("width", barWidth)
-      .attr("height", function (d) {
-        if (d.Data_Value === "~") {
-          d.Data_Value = 1;
-        }
-        console.log(height - yscale(parseFloat(d.Data_Value)));
-        return height - yscale(parseFloat(d.Data_Value));
-      })
-      .attr("y", function (d) {
-        return yscale(parseFloat(d.Data_Value));
-      })
-      .attr("x", function (d, i) {
-        // return (barWidth + barPadding) * i;
-        return xscale(d.Stratification1);
-      })
-      .attr("fill", "grey");
-
-    var timeBar = d3
-      .select("#chart-area")
-      .append("table")
-      .attr("id", "time-bar")
-      .attr("width", width + margin.left + margin.right)
-      .append("thead")
-      .append("tr");
-
-    var timeImg = timeBar.append("th").attr("id", "time-img");
-    var playIcon = timeImg.append("i").attr("class", "far fa-play-circle");
-
-    var timeTable = timeBar.append("th").attr("id", "time-table");
-    var timeCells = timeTable
-      .append("table")
-      .attr("id", "time-cells")
-      .attr("width", width + margin.right)
-      .append("thead");
-
-    var timeCell = timeCells
-      .append("tr")
-      .attr("id", "cells-row")
-      .selectAll("th")
-      .data(function (d, i) {
-        for (let j = minYear; j <= maxYear; j++) {
-          yearsArr.push(j);
-        }
-        return yearsArr;
-      })
-      .enter()
-      .append("th")
-      .attr("class", "time-cell")
-      .attr("data-cellvalue", function (d, i) {
-        return yearsArr[i];
-      });
-
-    var yearCell = timeCells
-      .append("tr")
-      .attr("id", "years-row")
-      .selectAll("th")
-      .data(yearsArr)
-      .enter()
-      .append("th")
-      .attr("class", "year-cell")
-      .text(function (d, i) {
-        return yearsArr[i];
-      });
-
-    timeCell.on("click", function () {
-      let year = parseInt(this.getAttribute("data-cellvalue"));
-
-      rects
-        .data(
-          data.filter(function (d) {
-            return parseInt(d.Description) === year;
+      var xscale = d3
+        .scaleBand()
+        .domain(
+          data.map(function (d) {
+            return d.Stratification1;
           })
         )
+        .range([0, width])
+        .paddingInner(0.3)
+        .paddingOuter(0.3);
+      var yscale = d3.scaleLinear().domain([0, maxPercent]).range([height, 0]);
+
+      var svg = d3
+        .select("#chart-area")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom);
+
+      var g = svg
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      g.append("text")
+        .attr("class", "x-axis-label")
+        .attr("x", width / 2)
+        .attr("y", height + margin.bottom - 20)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .text("Age Category(years)");
+
+      g.append("text")
+        .attr("class", "y-axis-label")
+        .attr("x", -(height / 2))
+        .attr("y", -60)
+        .attr("font-size", "20px")
+        .attr("text-anchor", "middle")
+        .attr("transform", "rotate(-90)")
+        .text("Obesity Percentage (%)");
+
+      var xAxis = d3.axisBottom(xscale);
+      g.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis)
+        .selectAll("text")
+        .attr("y", "10")
+        .attr("x", "-5")
+        .attr("text-anchor", "end")
+        .attr("transform", "rotate(-40)");
+
+      var yAxis = d3.axisLeft(yscale).tickFormat(function (d) {
+        return d + "%";
+      });
+      g.append("g").attr("class", "y-axis").call(yAxis);
+
+      var rects = g
+        .selectAll("rect")
+        .data(
+          data.filter(function (d) {
+            return parseInt(d.Description) === minYear;
+          })
+        )
+        .enter()
+        .append("rect")
+        .attr("width", barWidth)
         .attr("height", function (d) {
+          if (d.Data_Value === "~") {
+            d.Data_Value = 1;
+          }
+          console.log(height - yscale(parseFloat(d.Data_Value)));
           return height - yscale(parseFloat(d.Data_Value));
         })
         .attr("y", function (d) {
           return yscale(parseFloat(d.Data_Value));
+        })
+        .attr("x", function (d, i) {
+          // return (barWidth + barPadding) * i;
+          return xscale(d.Stratification1);
+        })
+        .attr("fill", "grey");
+
+      var timeBar = d3
+        .select("#chart-area")
+        .append("table")
+        .attr("id", "time-bar")
+        .attr("width", width + margin.left + margin.right)
+        .append("thead")
+        .append("tr");
+
+      var timeImg = timeBar.append("th").attr("id", "time-img");
+      var playIcon = timeImg.append("i").attr("class", "far fa-play-circle");
+
+      var timeTable = timeBar.append("th").attr("id", "time-table");
+      var timeCells = timeTable
+        .append("table")
+        .attr("id", "time-cells")
+        .attr("width", width + margin.right)
+        .append("thead");
+
+      var timeCell = timeCells
+        .append("tr")
+        .attr("id", "cells-row")
+        .selectAll("th")
+        .data(function (d, i) {
+          for (let j = minYear; j <= maxYear; j++) {
+            yearsArr.push(j);
+          }
+          return yearsArr;
+        })
+        .enter()
+        .append("th")
+        .attr("class", "time-cell")
+        .attr("data-cellvalue", function (d, i) {
+          return yearsArr[i];
         });
-    });
 
-    playIcon.on("click", function () {
-      let year = minYear;
-      let timer = setInterval(async function () {
-        if (year != minYear) {
-          document.getElementsByClassName("time-cell")[
-            yearsArr.indexOf(year - 1)
-          ].style["background-color"] = "grey";
-        }
+      var yearCell = timeCells
+        .append("tr")
+        .attr("id", "years-row")
+        .selectAll("th")
+        .data(yearsArr)
+        .enter()
+        .append("th")
+        .attr("class", "year-cell")
+        .text(function (d, i) {
+          return yearsArr[i];
+        });
 
-        if (year <= maxYear) {
-          document.getElementsByClassName("time-cell")[
-            yearsArr.indexOf(year)
-          ].style["background-color"] = "rgb(90, 87, 87)";
-        }
+      timeCell.on("click", function () {
+        let year = parseInt(this.getAttribute("data-cellvalue"));
+
         rects
           .data(
             data.filter(function (d) {
@@ -199,13 +272,42 @@ fetch("http://localhost:3001/api/ethnicity?country=Alaska")
           .attr("y", function (d) {
             return yscale(parseFloat(d.Data_Value));
           });
-        year++;
-        if (year > maxYear + 1) {
-          clearInterval(timer);
-        }
-      }, 1000);
+      });
+
+      playIcon.on("click", function () {
+        let year = minYear;
+        let timer = setInterval(async function () {
+          if (year != minYear) {
+            document.getElementsByClassName("time-cell")[
+              yearsArr.indexOf(year - 1)
+            ].style["background-color"] = "grey";
+          }
+
+          if (year <= maxYear) {
+            document.getElementsByClassName("time-cell")[
+              yearsArr.indexOf(year)
+            ].style["background-color"] = "rgb(90, 87, 87)";
+          }
+          rects
+            .data(
+              data.filter(function (d) {
+                return parseInt(d.Description) === year;
+              })
+            )
+            .attr("height", function (d) {
+              return height - yscale(parseFloat(d.Data_Value));
+            })
+            .attr("y", function (d) {
+              return yscale(parseFloat(d.Data_Value));
+            });
+          year++;
+          if (year > maxYear + 1) {
+            clearInterval(timer);
+          }
+        }, 1000);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+}
