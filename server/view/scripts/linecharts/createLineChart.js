6b55a3c0ea7531aa01d4c229db2d43786365eb78
Raluca Filimon <filimon.raluca@gmail.com>
2020-05-05 18:06:46 +0300

    added tootil to linechart

--- server/view/scripts/linecharts/createLineChart.js
+++ server/view/scripts/linecharts/createLineChart.js
@@ -1,3 +1,11 @@
+function sortByProperty(property) {
+  return function (a, b) {
+    if (a[property] > b[property]) return 1;
+    else if (a[property] < b[property]) return -1;
+
+    return 0;
+  };
+}
 async function createLineChart(seriesName, country, seriesValue) {
   url =
     "http://localhost:3001/api/" +
@@ -8,26 +16,16 @@ async function createLineChart(seriesName, country, seriesValue) {
     seriesName +
     "=" +
     seriesValue;
-  /*url = "http://localhost:3001/api/age?country=Alaska&age=18%20-%2024";*/
   fetch(url)
     .then((data) => {
       return data.json();
     })
     .then(async function (res) {
       data = res.data;
-      function sortByProperty(property) {
-        return function (a, b) {
-          if (a[property] > b[property]) return 1;
-          else if (a[property] < b[property]) return -1;
-
-          return 0;
-        };
-      }
       data.sort(sortByProperty("Description"));
       var maxPercent = d3.max(data, function (d) {
         return parseFloat(d.Data_Value);
       });
-      years = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017];
       maxPercent += 10;
       var margin = { left: 100, right: 100, top: 10, bottom: 180 };
       var width = 500 - margin.left - margin.right;
@@ -42,7 +40,7 @@ async function createLineChart(seriesName, country, seriesValue) {
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-      var x = d3.scaleTime().domain([2011, 2019]).range([0, width]);
+      var x = d3.scaleTime().domain([2011, 2018]).range([0, width]);
 
       let xAxis = d3.axisBottom(x).tickFormat(d3.format("d"));
       svg
@@ -56,7 +54,10 @@ async function createLineChart(seriesName, country, seriesValue) {
         .attr("transform", "rotate(-40)");
 
       var y = d3.scaleLinear().domain([0, maxPercent]).range([height, 0]);
-      svg.append("g").call(d3.axisLeft(y));
+      let yAxis = d3.axisLeft(y).tickFormat(function (d) {
+        return d + "%";
+      });
+      svg.append("g").call(yAxis);
 
       var bisectLeft = d3.bisector(function (d) {
         return d.Description;
@@ -73,12 +74,22 @@ async function createLineChart(seriesName, country, seriesValue) {
           return "steelblue";
         });
 
-      var focusText = svg
-        .append("g")
-        .append("text")
-        .style("opacity", 0)
-        .attr("text-anchor", "left")
-        .attr("alignment-baseline", "middle");
+      var focusText = d3
+        .select("#chart-area")
+        .data(data)
+        .append("div")
+        .style("position", "absolute")
+        .style("color", "#ffffff")
+        .style("z-index", "10")
+        .style("display", "none")
+        .style("background", "#125682")
+        .style("border", "0px solid #ffffff")
+        .style("border-radius", "5px")
+        .style("padding", "10px")
+        .style("font-size", "14px")
+        .text(function (d) {
+          return d.Data_Value;
+        });
 
       svg
         .append("text")
@@ -103,9 +114,14 @@ async function createLineChart(seriesName, country, seriesValue) {
 
       /*----create line chart---- */
 
-      svg
+      var linepath = svg
         .append("path")
         .datum(data)
+        .style("pointer-events", "all")
+        .on("mouseover", mouseover)
+        .on("mousemove", mousemove)
+        .on("mouseout", mouseout)
+        .attr("id", "pathID")
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-width", 1.5)
@@ -123,45 +139,39 @@ async function createLineChart(seriesName, country, seriesValue) {
               return y(parseFloat(d.Data_Value));
             })
         );
+      var pathbox = linepath.node().getBBox();
 
-      // Create a rect on top of the svg area: this rectangle recovers mouse position
-      svg
-        .append("rect")
-        .style("fill", "none")
-        .style("pointer-events", "all")
-        .attr("width", width)
-        .attr("height", height)
-        .on("mouseover", mouseover)
-        .on("mousemove", mousemove)
-        .on("mouseout", mouseout);
-
-      // What happens when the mouse move -> show the annotations at the right positions.
       function mouseover() {
         focus.style("opacity", 1);
         focusText.style("opacity", 1);
+        focusText.style("display", "block");
       }
 
       function mousemove() {
         var x0 = x.invert(d3.mouse(this)[0]);
-        var i = bisectLeft(data, x0);
-        selectedData = data[i];
+        var i = bisectLeft(data, x0, 1);
+        console.log(d3.mouse(this)[0]);
+        if (d3.mouse(this)[0] >= 270) selectedData = data[7];
+        else if (i > 0) {
+          (d0 = data[i - 1]), (d1 = data[i]);
+          selectedData = x0 - d0.Description > d1.Description - x0 ? d1 : d0;
+        } else {
+          selectedData = data[0];
+        }
         focus
           .attr("cx", x(selectedData.Description))
           .attr("cy", y(selectedData.Data_Value));
         focusText
-          .html(
-            "x:" +
-              selectedData.Description +
-              "  -  " +
-              "y:" +
-              selectedData.Data_Value
+          .text(
+            selectedData.Description + " - " + selectedData.Data_Value + "%"
           )
-          .attr("x", x(selectedData.Description) + 15)
-          .attr("y", y(selectedData.Data_Value));
+          .style("top", d3.event.pageY - 30 + "px")
+          .style("left", d3.event.pageX + 10 + "px");
       }
       function mouseout() {
         focus.style("opacity", 0);
         focusText.style("opacity", 0);
+        focusText.style("display", "none");
       }
 
       downloads(d3.select("svg"), data);
